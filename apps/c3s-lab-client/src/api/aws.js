c400ec66fb498ff6393c82b06cad9e5dc7a3af09
Daniel Böhler <daniel.boehler@uzh.ch>
2023-03-29 16:33:08 +1100

    removed submodules and moved frontend to own orchestration

--- apps/c3s-lab-client/src/api/aws.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-axios.defaults.headers['Content-Type'] = 'application/json'
-
-export default {
-  getBucketLinks (bucketName) {
-    return axios.get(process.env.BASE_ENDPOINT_URL + 'amazon/bucket/' + bucketName)
-  }
-}
